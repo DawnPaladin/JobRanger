b05a6ed3ef82b2aa6052b00decd4e482158fe9a5
James Harris <computersherpa@gmail.com>
2022-07-11 19:52:41 -0500

    create PointCounter component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import StatClicker from './StatClicker';
+import PointCounter from './PointCounter';
 
 const App = (props) => {
-	return <div className="stat-clickers">
-		<div className="column">
-			<StatClicker stat='STR' taskName='Job application' dailyCount={0} />
-			<StatClicker stat='DEX' taskName='Resume' dailyCount={0} />
-			<StatClicker stat='CON' taskName='Cover letter' dailyCount={0} />
+	return <main>
+		<div className="stat-clickers">
+			<div className="column">
+				<StatClicker stat='STR' taskName='Job application' dailyCount={0} />
+				<StatClicker stat='DEX' taskName='Resume' dailyCount={0} />
+				<StatClicker stat='CON' taskName='Cover letter' dailyCount={0} />
+			</div>
+			<div className="column">
+				<StatClicker stat='INT' taskName='Coding' dailyCount={0} />
+				<StatClicker stat='WIS' taskName='Searching' dailyCount={0} />
+				<StatClicker stat='CHA' taskName='Contact' dailyCount={0} />
+			</div>
 		</div>
-		<div className="column">
-			<StatClicker stat='INT' taskName='Coding' dailyCount={0} />
-			<StatClicker stat='WIS' taskName='Searching' dailyCount={0} />
-			<StatClicker stat='CHA' taskName='Contact' dailyCount={0} />
+		<div className="point-counters">
+			<PointCounter name="Today's points:" points={100} highScore={100}/>
+			<PointCounter name="Week points:" points={100} highScore={100}/>
 		</div>
-	</div>
+	</main>
 }
 
 export default App;
---
+++ app/javascript/components/PointCounter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const PointCounter = props => {
+	const { name, points, highScore } = props;
+	return <div className="point-counter">
+		<div className="name">{name}</div>
+		<div className="points">{points}</div>
+		<div className="high-score">High score: {highScore}</div>
+	</div>
+}
+PointCounter.propTypes = {
+	name: PropTypes.string.isRequired,
+	points: PropTypes.number.isRequired,
+	highScore: PropTypes.number.isRequired
+}
+
+export default PointCounter;

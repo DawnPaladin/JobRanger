7545a89f549ba49cf16a1b1e5569cb204e6d1b5d
James Harris <computersherpa@gmail.com>
2022-07-15 20:15:34 -0500

    Create other stats in the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,10 @@ Stat.destroy_all
 
 p "Create stats"
 Stat.create(name: "STR")
+Stat.create(name: "CON")
+Stat.create(name: "WIS")
+Stat.create(name: "DEX")
+Stat.create(name: "INT")
+Stat.create(name: "CHA")
 
 p "Done"

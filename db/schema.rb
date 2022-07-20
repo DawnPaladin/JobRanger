14202fa88b969b5d4b1650aef55e311abb5d5ae7
James Harris <computersherpa@gmail.com>
2022-07-19 23:12:48 -0500

    Set default value of XP records to 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_030535) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_20_041050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,7 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_030535) do
   create_table "xps", force: :cascade do |t|
     t.string "name"
     t.string "date"
-    t.integer "value"
+    t.integer "value", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_weekly", default: false
